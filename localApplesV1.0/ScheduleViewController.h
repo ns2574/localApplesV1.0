8c9ce222dd29f96849fc9954895b5776327209b1
ns2574 <ns2574@nyu.edu>
2016-04-09 03:17:32 -0400

    new stuff
    add stuff

---
+++ localApplesV1.0/ScheduleViewController.h
@@ -0,0 +1,43 @@
+//
+//  ScheduleViewController.h
+//  localApplesV1.0
+//
+//  Created by Niela Sultana on 4/9/16.
+//  Copyright © 2016 Niela Sultana. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ScheduleViewController : ViewController
+
+@property (weak, nonatomic) IBOutlet UITextField *mondayEntry;
+
+@property (weak, nonatomic) IBOutlet UITextField *mondayExit;
+
+@property (weak, nonatomic) IBOutlet UITextField *tuesdayEntry;
+
+@property (weak, nonatomic) IBOutlet UITextField *tuesdayExit;
+
+@property (weak, nonatomic) IBOutlet UITextField *wednesdayEntry;
+
+@property (weak, nonatomic) IBOutlet UITextField *wednesdayExit;
+
+@property (weak, nonatomic) IBOutlet UITextField *thursdayEntry;
+
+@property (weak, nonatomic) IBOutlet UITextField *thursdayExit;
+
+@property (weak, nonatomic) IBOutlet UITextField *fridayEntry;
+
+@property (weak, nonatomic) IBOutlet UITextField *fridayExit;
+
+@property (weak, nonatomic) IBOutlet UITextField *saturdayEntry;
+
+@property (weak, nonatomic) IBOutlet UITextField *saturdayExit;
+
+@property (weak, nonatomic) IBOutlet UITextField *sundayEntry;
+
+
+
+
+
+@end
